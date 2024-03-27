e45d1b0e8c1669530d03609efc0514fa8acfd330
Vasyl Maslov <maslovvasil@gmail.com>
2024-03-27 17:06:21 +0200

    add files gallery
    add files gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox'
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css'
+import { galleryItems } from './gallery-items'
+
 // Change code below this line
 
-console.log(galleryItems);
+console.log(galleryItems)
+
+const galleryContainer = document.querySelector('.gallery')
+
+const itemsMarkup = createGalleryItemsMarkup(galleryItems)
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup)
+
+// rendered items
+function createGalleryItemsMarkup (items) {
+  return items.map(({ preview, original, description }) => {
+    return `<li>
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+  }).join('')
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
+})
+
+// const lightbox = new SimpleLightbox('.gallery a', {
+//   captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
+// })
